1a2acf97887d0a10358a0a2eb029f20c05ea6dfa
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2023-12-14 11:21:47 +0100

    maggiore ordine file

--- run.cmd
+++ run.cmd
@@ -8,7 +8,7 @@ start cmd /c docker run -it -v "C:\Users\Davide Rizzi\IdeaProjects\cacert-gen:/h
 echo Copiarsi la password dall'altra schermata quindi premere invio
 PAUSE >nul
 docker run -it -v "C:\Users\Davide Rizzi\IdeaProjects\cacert-gen:/home/step" ^
-	--entrypoint /home/step/script.sh --name jq stedolan/jq
+	--entrypoint /home/step/resources/script.sh --name jq stedolan/jq
 docker restart step
 docker exec -it step step ca certificate "nethical.vitaever.localhost" --not-after=26280h /home/step/cert/client.crt /home/step/cert/client.key
 docker exec -it step step ca root /home/step/cert/ca.crt
--- script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-jq -s ".[0] * .[1]" /home/step/data.json /home/step/config/ca.json > /home/step/config/ca.json
