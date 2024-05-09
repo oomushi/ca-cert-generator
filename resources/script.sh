658f6a68ec09ea61658129ad805dc0983df588b6
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2024-05-09 12:57:45 +0200

    script in due step per non avere casini

--- resources/script.sh
+++ resources/script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-jq -s ".[0] * .[1]" /home/step/resources/data.json /home/step/config/ca.json > /home/step/config/ca.json
+jq -s ".[0] * .[1]" /home/step/resources/data.json /home/step/config/ca.json > /home/step/config/tmp.json
+mv /home/step/config/tmp.json /home/step/config/ca.json
