4060594d81e0d43b6f741e52d3976fb2200d25c4
Davide Rizzi <150713+sadjehwty@users.noreply.github.com>
2024-05-28 14:36:31 +0200

    aggiornato con container più funzionanti

--- resources/script.sh
+++ resources/script.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 jq -s ".[0] * .[1]" /home/step/resources/data.json /home/step/config/ca.json > /home/step/config/tmp.json
 mv /home/step/config/tmp.json /home/step/config/ca.json
